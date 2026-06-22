103075179bd3371015bf7bce69b98bdf55c7b4cc
IntenseWiggling <iw@wiggling.me>
2026-06-22 08:43:05 -0400

    Add support for building and uploading left-half ELF firmware

--- config/default.nix
+++ config/default.nix
@@ -8,4 +8,12 @@ let
   go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
   go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
 
-in firmware.combine_uf2 go60_left go60_right "go60"
+  go60_left_elf = go60_left.overrideAttrs (old: {
+    postInstall = (old.postInstall or "") + ''
+      find . -name 'zmk.elf' -exec cp {} $out/zmk.elf \;
+    '';
+  });
+
+in (firmware.combine_uf2 go60_left go60_right "go60") // {
+  inherit go60_left_elf;
+}
